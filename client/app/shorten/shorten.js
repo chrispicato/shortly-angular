60375bb0f87d04d796130341dedaf54b3091b7b2
chrispicato <christopherpicato@gmail.com>
2016-08-26 19:05:17 -0700

    Authenticate the right way

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -2,9 +2,9 @@ angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links, Auth) {
   // Your code her
-  if (!Auth.isAuth()) {
-    $location.url('/signin');
-  }
+  // if (!Auth.isAuth()) {
+  //   $location.url('/signin');
+  // }
   $scope.link = {};
 
   $scope.addLink = function () {
